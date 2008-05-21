b5686df4fa98193825f278f34b4e8ba75742ce31
dmaciej1 <dmaciej1@f75eed02-8a0f-11dd-b20b-83d96e936561>
2008-05-21 10:57:34 +0000

    Wersja 27 Wstęp do shadow volumes, multipass lights

---
+++ CamFighter/Math/xLight.cpp
@@ -0,0 +1,43 @@
+#include "xLight.h"
+
+xFLOAT staticCalculateIntensity ( xFLOAT distance, const xVector3& attenuation )
+{
+    return 1.0 / (attenuation.x + attenuation.y * distance + attenuation.z * distance * distance);
+}
+
+xFLOAT staticCalculateRadius(xFLOAT testIncrement, const xVector3& attenuation)
+{
+    float testRadius = testIncrement;
+    static const xFLOAT minIntensity = 10.f / 256.f;
+    static const xFLOAT minIntensityI = 25.6f;
+
+    // make sure the parameters are reasonable and that
+    // the algorithm will terminate
+    if ((attenuation.x < 0.f) ||
+        (attenuation.y < 0.f) ||
+        (attenuation.z < 0.f) ||
+        ((attenuation.x < minIntensityI) && (attenuation.y == 0.f) && (attenuation.z == 0.f)))
+        return -1.0;
+
+    while (staticCalculateIntensity(testRadius, attenuation) > minIntensity)
+        testRadius += testIncrement;
+
+    return testRadius;
+}
+
+void xLight::update()
+{
+    if (modified)
+    {
+        if (type == xLight_INFINITE) { radius = -1.f; return; }
+        radius = staticCalculateRadius(0.1f, xVector3::Create(attenuationConst, attenuationLinear, attenuationSquare));
+        boundingPoints[0].init(position.x - radius, position.y - radius, position.z - radius);
+        boundingPoints[1].init(position.x + radius, position.y + radius, position.z + radius);
+        boundingPoints[2].init(position.x + radius, position.y - radius, position.z - radius);
+        boundingPoints[3].init(position.x - radius, position.y + radius, position.z - radius);
+        boundingPoints[4].init(position.x + radius, position.y + radius, position.z - radius);
+        boundingPoints[5].init(position.x - radius, position.y - radius, position.z + radius);
+        boundingPoints[6].init(position.x + radius, position.y - radius, position.z + radius);
+        boundingPoints[7].init(position.x - radius, position.y + radius, position.z + radius);
+    }
+}
---
+++ CamFighter/Math/xLight.h
@@ -0,0 +1,46 @@
+#ifndef __incl_lib3dx_xLight_h
+#define __incl_lib3dx_xLight_h
+
+#include "xFieldOfView.h"
+
+typedef enum
+{
+    xLight_POINT    = 1,
+    xLight_SPOT     = 2,
+    xLight_INFINITE = 3
+} xLightType;
+
+struct xLight
+{
+    char*       name;
+    char*       prefix;			// Light prefix
+
+    bool        turned_on;
+
+    xLightType  type;
+    xVector3    position;
+    xColor      color;
+
+    xVector3    spotDirection;
+    xFLOAT      spotCutOff;
+    xFLOAT      spotAttenuation; // spot attenuation 0-no attenuation 128-max
+
+    xFLOAT      attenuationConst;
+    xFLOAT      attenuationLinear;
+    xFLOAT      attenuationSquare;
+
+    bool        modified;
+
+    xFLOAT      radius;
+    xVector3    boundingPoints[8];
+
+    void update();
+
+    bool isVisible(xFieldOfView *FOV)
+    {
+        if (type == xLight_INFINITE || radius < 0) return true;
+        return FOV->CheckBox(boundingPoints);
+    }
+};
+
+#endif
