74eb80a67be9ecb8cc9d2f6367b7741da3e8008b
dmaciej1 <dmaciej1@f75eed02-8a0f-11dd-b20b-83d96e936561>
2008-09-13 14:06:22 +0000

    Missing files

---
+++ CamFighter/Graphics/Textures/TextureMgr.cpp
@@ -0,0 +1,30 @@
+#include "TextureMgr.h"
+#include "../../Utils/Filesystem.h"
+
+HTexture TextureMgr :: GetTexture( const char* name )
+{
+    // insert/find
+    NameIndexInsertRc rc =
+        m_NameIndex.insert( std::make_pair( Filesystem::GetFullPath(name), HTexture() ) );
+    if ( rc.second )
+    {
+        // this is a new insertion
+        Graphics::OGL::Texture* tex = m_HandleMgr.Acquire( rc.first->second );
+        if ( !tex->Create( rc.first->first, true ) )
+        {
+            m_HandleMgr.Release( rc.first->second );
+            m_NameIndex.erase( rc.first );
+            //assert (false);
+            return HTexture();
+        }
+    }
+    Lock(rc.first->second);
+
+    if (IsHandleValid(rc.first->second))
+        return ( rc.first->second );
+    else
+    {
+        assert (false);
+        return HTexture();
+    }
+}
---
+++ CamFighter/Graphics/Textures/TextureMgr.h
@@ -0,0 +1,30 @@
+#ifndef __incl_TextureMgr_h
+#define __incl_TextureMgr_h
+
+#include "../../Utils/Singleton.h"
+#include "../../Utils/Manager.h"
+#include "../OGL/Texture.h"
+
+#define g_TextureMgr TextureMgr::GetSingleton()
+
+class TextureMgr;
+typedef Handle <TextureMgr> HTexture;
+
+class TextureMgr : public Singleton<TextureMgr>, public Manager<Graphics::OGL::Texture, HTexture>
+{
+public:
+// Texture management.
+    HTexture GetTexture   ( const char* name );
+
+// Texture query.
+    const std::string& GetName( HTexture htex ) const
+        {  return ( m_HandleMgr.DereferenceNoValidation( htex )->Name );  }
+    int GetWidth( HTexture htex ) const
+        {  return ( m_HandleMgr.DereferenceNoValidation( htex )->Width );  }
+    int GetHeight( HTexture htex ) const
+        {  return ( m_HandleMgr.DereferenceNoValidation( htex )->Height );  }
+    void BindTexture( HTexture htex )
+        { m_HandleMgr.Dereference( htex )->Bind(); }
+};
+
+#endif
